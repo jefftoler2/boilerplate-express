64ed6d5efc88777ecb105439ab9fcc63affc20b6
Jeffrey Toler <jefftoler2@gmail.com>
2024-02-09 22:33:01 +0000

    poop

--- myApp.js
+++ myApp.js
@@ -1,39 +1,15 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/json", (req, res) => {
+  let response = "Hello json";
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    response = response.toLocaleLowerCase();
+  }
+  res.json({ message: response });
+});
+module.exports = app;
